e87c93786ea1f525bca636e557f8e6aadc821f50
JothiSL <105314622+jothids@users.noreply.github.com>
2025-05-20 23:23:50 +0530

    Create centos-8-stream.tf

---
+++ prerequisites/centos-8-stream.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1" # Change as needed
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+resource "aws_instance" "centos_stream8" {
+  ami           = "ami-05f2b469e504202f7"
+  instance_type = "t3.nano"
+  key_name      = "Nvirginia" # Replace with your key pair name
+  tags = {
+    Name = "CentOS-Stream8-Instance"
+  }
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+}
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.centos_stream8.public_ip
+}
